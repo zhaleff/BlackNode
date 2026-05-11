577093af249517c78e0b1e5473366bcf7a1efe8b
zhaleff <sypherpruebas@gmail.com>
2026-05-11 15:35:17 -0600

    configuration update
    I've been improving every configuration to offer the best possible experience in this dotfile. I changed the styles of some rofis and improved the scripts. The improved scripts were some from .local/bin/ and .config/rofi/.

--- Configs/.config/rofi/wallselect/script.sh
+++ Configs/.config/rofi/wallselect/script.sh
@@ -2,11 +2,7 @@
 
 WALL_DIR="$HOME/.local/share/wallpapers"
 
-SELECTED=$(find "$WALL_DIR" -type f \( \
-    -iname "*.jpg" -o \
-    -iname "*.jpeg" -o \
-    -iname "*.png" -o \
-    -iname "*.gif" \) \
+SELECTED=$(find "$WALL_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) \
     | while read -r img; do
         printf '%s\0icon\x1f%s\n' "$img" "$img"
       done \
@@ -19,15 +15,11 @@ if ! pgrep -x "awww" > /dev/null; then
     awww &
     sleep 0.2
 fi
-
 awww img "$SELECTED" --transition-type=random
 
-# Solo copia a wallust si NO es gif (porque wallust no maneja bien gifs)
-if [[ "$SELECTED" != *.gif ]]; then
-    cp "$SELECTED" ~/.cache/wallust/current_wallpaper.png
-    wallust run ~/.cache/wallust/current_wallpaper.png
-    wallust run "$SELECTED"
-fi
+cp "$SELECTED" ~/.cache/wallust/current_wallpaper.png
+
+wallust run "$SELECTED"
 
 killall -SIGUSR2 waybar && killall dunst && dunst &
 pkill -USR1 cava
