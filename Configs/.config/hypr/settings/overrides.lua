7cceac5e14224000fca07b9565fd748913ad1c28
zhaleff <sypherpruebas@gmail.com>
2026-07-10 20:28:44 -0600

    overrides: initial override file matching decoration/general/animations defaults

---
+++ Configs/.config/hypr/settings/overrides.lua
@@ -0,0 +1,16 @@
+hl.config({
+    decoration = {
+        rounding = 10,
+        dim_special = 0.3,
+        blur = { enabled = true },
+        shadow = { enabled = false },
+    },
+    general = {
+        gaps_in = 3,
+        gaps_out = 7,
+        border_size = 1,
+    },
+    animations = {
+        enabled = true,
+    },
+})
