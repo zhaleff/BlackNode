73c2d2bbdf7748dc24d741d387e22ef4824938f3
zhaleff <sypherpruebas@gmail.com>
2026-07-18 12:08:49 -0600

    idk

---
+++ Configs/.config/nvim/generated.lua
@@ -0,0 +1,26 @@
+-- ~/.config/matugen/templates/nvim.lua
+return {
+  colors = {
+    comment = "#8b9198",
+    delimiter = "#8b9198",
+    operator = "#94cdf7",
+    todo = "#b7c9d9",
+    identifier = "#94cdf7",
+    constant = "#cec0e8",
+    type = "#b7c9d9",
+    string = "#cec0e8",
+    special = "#b7c9d9",
+    preproc = "#b7c9d9",
+    function_name = "#94cdf7",
+    statement = "#94cdf7",
+    error_bg = "#ffb4ab",
+    error_fg = "#690005",
+    status_bg = "#94cdf7",
+    status_fg = "#00344d",
+    status_nc_bg = "#41474d",
+    status_nc_fg = "#c1c7ce",
+    visual_bg = "#004c6e",
+    normal_bg = "#101417",
+    normal_fg = "#dfe3e8"
+  }
+}
